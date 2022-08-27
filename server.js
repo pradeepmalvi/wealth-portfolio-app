f586f8225abc3f15df0a8f65ee29d7b2f77ffd0f
Pradeep Malviya <p.malviyaaa@gmail.com>
2022-08-27 11:20:05 +0530

    newrelic package update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { connectDB } = require("./config/db");
+require("newrelic");
 
 const userRoutes = require("./routes/route");
 
